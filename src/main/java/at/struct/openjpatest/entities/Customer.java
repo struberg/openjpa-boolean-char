9bf8c1f0bacfd21a1c003eb86312e3ff5c74bcc0
Mark Struberg <struberg@apache.org>
2015-01-10 00:43:49 +0100

    setup a small openjpa test for CHAR(1) boolean handling
    When you run the build you can see in the log that
    OpenJPA passes > (int)1 < as parameter into the query.
    This internally causes a toNumber in Oracle and h2 which
    leads to not taking any indices...

---
+++ src/main/java/at/struct/openjpatest/entities/Customer.java
@@ -0,0 +1,45 @@
+package at.struct.openjpatest.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="CUSTOMER")
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    private Long id;
+
+    private String name;
+
+    private Boolean active;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
---
+++ src/test/java/at/struct/openjpatest/OpenJpaTest.java
@@ -0,0 +1,24 @@
+package at.struct.openjpatest;
+
+import java.util.List;
+
+import at.struct.openjpatest.entities.Customer;
+import junit.framework.Assert;
+import org.testng.annotations.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class OpenJpaTest {
+
+    @Test
+    public void testOpenJpa() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("MYAPP");
+        EntityManager em = emf.createEntityManager();
+
+        List<Customer> resultList = em.createQuery("select c from Customer AS c where c.active = :active", Customer.class).setParameter("active", true).getResultList();
+        Assert.assertNotNull(resultList);
+        Assert.assertEquals(3, resultList.size());
+    }
+}
